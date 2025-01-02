e9db97e18e6cb9772957ba8f88b4e1b31ba4d128
Md Abdur Rahim <98558957+marahim34@users.noreply.github.com>
2025-01-02 11:44:56 +0200

    Issues with marging and responsiveness fixed

--- js/portfolio.js
+++ js/portfolio.js
@@ -6,14 +6,11 @@ function menuToggle() {
 function showExperience(companyId) {
   const companyListItems = document.querySelectorAll(".company-list li");
   companyListItems.forEach((item) => item.classList.remove("active"));
-
   const activeItem = document.querySelector(
     `[onclick="showExperience('${companyId}')"]`
   );
   activeItem.classList.add("active");
-
   const experienceDetails = document.querySelectorAll(".experience-details");
   experienceDetails.forEach((detail) => detail.classList.remove("active"));
-
   document.getElementById(companyId).classList.add("active");
 }
